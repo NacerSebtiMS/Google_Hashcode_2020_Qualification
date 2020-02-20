7a54235fba36f51c017dfed73563c45948612028
Nacer Sebti <nacersebti.ms@gmail.com>
2020-02-20 20:18:03 +0100

    copy

--- SIN_solution.py
+++ SIN_solution.py
@@ -46,3 +46,23 @@ def read_input(file):
     return int(B),int(L),int(D), BL, LL
 
 B,L,D,BL,LL = read_input(FILE)
+
+def scorify_library(library):
+    """
+    The aim is to give the libraries a score, that will enable to order them later on
+    """
+    NB = library[0]
+    BD = library[2]
+    SB = library_total_book_score(library)
+    DR = library[1]
+    library_scoring = (D - DR) * BD * (SB/NB)
+    return library_scoring
+
+
+
+def library_total_book_score(library):
+    book_ids = library[3]
+    total_library_book_score = 0
+    for id in book_ids:
+        total_library_book_score += BL[id]
+    return total_library_book_score
